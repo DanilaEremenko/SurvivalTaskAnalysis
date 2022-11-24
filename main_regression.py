4d03c02d602243d0686940c4c277a7e74fb62fa2
Danila <danilaeremenko@mail.ru>
2022-11-25 02:56:46 +0300

    STA: main_regression.py simplified

--- main_regression.py
+++ main_regression.py
@@ -3,15 +3,15 @@ from datetime import timedelta
 
 from pathlib import Path
 import pandas as pd
+from joblib import load, dump
 from pandas.api.types import is_string_dtype
 
 from sklearn.ensemble import RandomForestRegressor
-from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
 from typing import Dict, List
 
 from drawing import draw_group_bars_and_boxes, draw_corr_sns, draw_pie_chart
-from models_building import get_reg_predictions_and_metrics_df, build_scenarios
+from models_building import build_scenarios
 from time_ranges import get_time_range_symb
 
 
@@ -48,139 +48,88 @@ def draw_correlations(res_test_df: pd.DataFrame, res_dir: Path):
 
 
 class ExpRegDesc:
-    def __init__(self, res_dir: str, src_file: str, y_key: str, ignored_keys: List[str]):
+    def __init__(self, res_dir: str, train_file: str, test_file: str, y_key: str):
         self.res_dir = Path(res_dir)
-        self.src_file = src_file
+        self.train_file = train_file
+        self.test_file = test_file
         self.y_key = y_key
-        self.ignored_keys = ignored_keys
 
 
 if __name__ == '__main__':
     ################################################
     # ------------ exp descriptions  ---------------
     ################################################
-    exp_list = [
-        ExpRegDesc(res_dir='full_reg_cpu_time', src_file='sk-data/full_data.csv', y_key='CPUTimeRAW',
-                   ignored_keys=['ElapsedRaw', 'ElapsedRaw_mean', 'ElapsedRawClass', 'Unnamed: 0']),
 
-        ExpRegDesc(res_dir='full_reg_elapsed_time (super fair)', src_file='sk-data/full_data.csv',
-                   y_key='ElapsedRaw',
-                   ignored_keys=[
-                       'CPUTimeRAW', 'ElapsedRaw_mean', 'ElapsedRaw_std', 'ElapsedRawClass',
-                       'SizeClass',
-                       'AveCPU', 'AveCPU_mean', 'AveCPU_std',
-                       'Unnamed: 0', 'State', 'MinMemoryNode'
-                   ]),
+    exp_desc = ExpRegDesc(
+        res_dir='full_reg_elapsed_time (super fair)',
+        train_file='sk-full-data/fair_ds/train.csv',
+        test_file='sk-full-data/fair_ds/test.csv',
+        y_key='ElapsedRaw'
+    )
 
-    ]
-
-    exp_desc = exp_list[1]
     exp_desc.res_dir.mkdir(exist_ok=True)
 
-    ################################################
-    # ------------ data processing  ----------------
-    ################################################
-    src_df = pd.read_csv(exp_desc.src_file)
-
-    corr_df = src_df.corr(numeric_only=True)
-
-    # get most correlated with target value columns
-    # corr_df[exp_desc.y_key].sort_values()
-
-    filt_df = src_df.dropna(axis=1)
-
-    filt_ignored_keys = [key for key in exp_desc.ignored_keys if key in filt_df.keys()]
-    filt_df = filt_df.drop(columns=filt_ignored_keys)
-
-    filt_df = filt_df[filt_df[exp_desc.y_key] != 0]
+    train_df = pd.read_csv(exp_desc.train_file)
+    test_df = pd.read_csv(exp_desc.test_file)
 
     # initialize label encoders
     le_dict: Dict[str, LabelEncoder] = {
-        key: LabelEncoder() for key in filt_df.keys()
-        if is_string_dtype(filt_df[key])
+        key: LabelEncoder() for key in train_df.keys()
+        if is_string_dtype(train_df[key])
     }
 
-    x_all, y_all = filt_df.drop(columns=[exp_desc.y_key]), filt_df[exp_desc.y_key]
-
     for key, le in le_dict.items():
-        x_all[key] = le.fit_transform(x_all[key])
+        train_df[key] = le.fit_transform(train_df[key])
+        test_df[key] = le.fit_transform(test_df[key])
 
-    x_train, x_test, y_train, y_test = train_test_split(
-        x_all, y_all, test_size=0.33,
-        # shuffle=True
-    )
+    x_train, y_train = train_df.drop(columns=[exp_desc.y_key]), train_df[exp_desc.y_key]
+    x_test, y_test = test_df.drop(columns=[exp_desc.y_key]), test_df[exp_desc.y_key]
 
     ################################################
     # ------------ building models -----------------
     ################################################
-    res_list_df = build_scenarios(
-        x_train=x_train, y_train=y_train, x_test=x_test, y_test=y_test,
-        method='rf',
-        args_scenarios=[
-            dict(
-                n_estimators=n_estimators,
-                min_samples_leaf=min_samples_leaf,
-                bootstrap=bootstrap,
-                max_features=max_features,
-                n_jobs=4,
-                random_state=42
-            )
-            # # search
-            # for n_estimators in [10, 50, 150]
-            # for min_samples_leaf in [1, 2, 4]
-            # for bootstrap in [True, False]
-            # for max_features in [1.0, 0.75, 0.5, 0.25]
-
-            # stable
-            for n_estimators in [50]
-            for min_samples_leaf in [1]
-            for bootstrap in [False]
-            for max_features in [0.25]
-        ]
-    )
-
-    for key, le in le_dict.items():
-        res_list_df[key] = le.inverse_transform(res_list_df[key])
-
-    res_list_df.sort_values('mae', inplace=True)
-    res_list_df.to_csv(f'{exp_desc.res_dir}/res_last_search.csv')
-
+    # res_list_df = build_scenarios(
+    #     x_train=x_train, y_train=y_train, x_test=x_test, y_test=y_test,
+    #     method='rf',
+    #     args_scenarios=[
+    #         dict(
+    #             n_estimators=n_estimators,
+    #             min_samples_leaf=min_samples_leaf,
+    #             bootstrap=bootstrap,
+    #             max_features=max_features,
+    #             n_jobs=4,
+    #             random_state=42
+    #         )
+    #         # # search
+    #         # for n_estimators in [10, 50, 150]
+    #         # for min_samples_leaf in [1, 2, 4]
+    #         # for bootstrap in [True, False]
+    #         # for max_features in [1.0, 0.75, 0.5, 0.25]
+    #
+    #         # stable
+    #         for n_estimators in [50]
+    #         for min_samples_leaf in [1]
+    #         for bootstrap in [False]
+    #         for max_features in [0.25]
+    #     ]
+    # )
+    #
+    # res_list_df.sort_values('mae', inplace=True)
+    # res_list_df.to_csv(f'{exp_desc.res_dir}/res_last_search.csv')
+    #
+    # best_args = json.loads(res_list_df.iloc[0]['args_dict'])
+    #
+    # rf = RandomForestRegressor(**best_args)
+    # rf.fit(X=x_train, y=y_train)
+    #
+    # dump(rf, f'{exp_desc.res_dir}/model.joblib')
     ################################################
     # --- analyze model errors & dependencies  -----
     ################################################
-    best_args = json.loads(res_list_df.iloc[0]['args_dict'])
+    rf = load(f'{exp_desc.res_dir}/model.joblib')
 
-    rf = RandomForestRegressor(**best_args)
-    rf.fit(X=x_train, y=y_train)
+    imp_df = pd.DataFrame({'feature': x_test.keys(), 'imp': rf.feature_importances_}) \
+        .sort_values('imp', ascending=False)
 
-    imp_df = pd.DataFrame({'feature': x_test.keys(), 'imp': rf.feature_importances_}).sort_values('imp')
-
-    res_test_df = get_reg_predictions_and_metrics_df(model=rf, X=x_test, y=y_test)
-
-    for key, le in le_dict.items():
-        res_test_df[key] = le.inverse_transform(res_test_df[key])
-
-    res_test_df['time_range'] = [get_time_range_symb(task_time=task_time)
-                                 for task_time in list(res_test_df['y_true'])]
-
-    src_df['time_range'] = [get_time_range_symb(task_time=task_time)
-                            for task_time in list(src_df[exp_desc.y_key])]
-
-    # bars & boxes
-    draw_bars_and_boxes_by_categories(df=src_df, group_keys=list(le_dict.keys()), y_key=exp_desc.y_key,
-                                      res_dir=exp_desc.res_dir.joinpath('dist').joinpath('y_by_categories'))
-    draw_bars_and_boxes_by_categories(df=res_test_df, group_keys=list(le_dict.keys()), y_key='mae perc',
-                                      res_dir=exp_desc.res_dir.joinpath('dist').joinpath('test_error_by_categories'))
-    draw_bars_and_boxes_by_categories(df=res_test_df, group_keys=list(le_dict.keys()), y_key='y_pred',
-                                      res_dir=exp_desc.res_dir.joinpath('dist').joinpath('test_pred_by_categories'))
-    # correlation
-    draw_correlations(res_test_df=res_test_df, res_dir=exp_desc.res_dir.joinpath('corr'))
-
-    # pie chart dist
-    for pie_mode in ['sum', 'count']:
-        draw_pie_chart(
-            df=src_df,
-            group_key='time_range', sum_key=exp_desc.y_key,
-            mode=pie_mode,
-            res_path=f"{exp_desc.res_dir}/range_dist_mode={pie_mode}.html"
-        )
+    y_pred = pd.DataFrame({'y_pred': rf.predict(x_test)})
+    y_pred.to_csv('sk-full-data/fair_ds/y_pred_reg.csv', index=False)
