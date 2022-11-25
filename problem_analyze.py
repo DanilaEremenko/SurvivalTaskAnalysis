244ef73a5195a81111b17c8c63652315e2cfe599
Danila <danilaeremenko@mail.ru>
2022-11-25 04:40:07 +0300

    STA: survival model results added into problem_analyze.py

--- problem_analyze.py
+++ problem_analyze.py
@@ -4,6 +4,7 @@ from time_ranges import get_time_range_symb
 
 src_df = pd.read_csv('sk-full-data/fair_ds/test.csv')
 src_df['TimelimitReg'] = pd.read_csv('sk-full-data/fair_ds/y_pred_reg.csv')['y_pred']
+src_df['TimelimitSurv'] = pd.read_csv('sk-full-data/fair_ds/y_pred_surv.csv')['y_pred']
 
 src_df['time_elapsed_range'] = [get_time_range_symb(task_time=task_time)
                                 for task_time in list(src_df['ElapsedRaw'])]
@@ -13,6 +14,9 @@ src_df['time_limit_range'] = [get_time_range_symb(task_time=task_time)
 
 src_df['time_limit_reg_range'] = [get_time_range_symb(task_time=task_time)
                                   for task_time in list(src_df['TimelimitReg'])]
+
+src_df['time_limit_surv_range'] = [get_time_range_symb(task_time=task_time)
+                                   for task_time in list(src_df['TimelimitSurv'])]
 
 time_elapsed_agg_df = pd.DataFrame([
     {
@@ -32,8 +36,12 @@ for gt_limit in src_df['time_elapsed_range'].unique():
     for pred_limit in src_df['time_elapsed_range'].unique():
         curr_dumb_df = curr_el_df[curr_el_df['time_limit_range'] == pred_limit]
         curr_dumb_gap = (curr_dumb_df['TimelimitRaw'] - curr_dumb_df['ElapsedRaw']).sum()
+
         curr_reg_df = curr_el_df[curr_el_df['time_limit_reg_range'] == pred_limit]
         curr_reg_gap = (curr_reg_df['TimelimitReg'] - curr_reg_df['ElapsedRaw']).sum()
+
+        curr_surv_df = curr_el_df[curr_el_df['time_limit_surv_range'] == pred_limit]
+        curr_surv_gap = (curr_reg_df['TimelimitSurv'] - curr_reg_df['ElapsedRaw']).sum()
 
         confusion_df.append(
             {
@@ -42,7 +50,9 @@ for gt_limit in src_df['time_elapsed_range'].unique():
                 'tasks dumb (part)': len(curr_dumb_df) / len(curr_el_df),
                 'gap dumb (part)': curr_dumb_gap / SUMMARY_GAP,
                 'tasks reg (part)': len(curr_reg_df) / len(curr_el_df),
-                'gap reg (part)': curr_reg_gap / SUMMARY_GAP
+                'gap reg (part)': curr_reg_gap / SUMMARY_GAP,
+                'tasks surv (part)': len(curr_surv_df) / len(curr_el_df),
+                'gap surv (part)': curr_surv_gap / SUMMARY_GAP
             }
         )
 
