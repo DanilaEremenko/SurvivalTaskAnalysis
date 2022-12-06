f23fceea72743e46264d1c5dc6b480a95d345145
Danila <danilaeremenko@mail.ru>
2022-12-07 01:24:30 +0300

    STA: prepare_ds.py updated

--- prepare_ds.py
+++ prepare_ds.py
@@ -3,7 +3,7 @@ from pathlib import Path
 import pandas as pd
 from sklearn.model_selection import train_test_split
 
-src_df = pd.read_csv('sk-full-data/full_data.csv')
+src_df = pd.read_csv('sk-full-data/full_data.csv', index_col=0)
 res_dir = Path('sk-full-data/fair_ds')
 res_dir.mkdir(exist_ok=True)
 
@@ -19,10 +19,10 @@ filt_df = src_df.dropna(axis=1)
 
 filt_ignored_keys = [key for key in ignored_keys if key in filt_df.keys()]
 filt_df = filt_df.drop(columns=filt_ignored_keys)
-filt_df = filt_df[filt_df['ElapsedRaw'] > 0]
-filt_df = filt_df[[key for key in filt_df.keys()]]
+# filt_df = filt_df[filt_df['ElapsedRaw'] > 0]
+# filt_df = filt_df[[key for key in filt_df.keys() if 'License' not in key]]
 
 train_df, test_df = train_test_split(filt_df, test_size=0.33, random_state=42)
 
-train_df.to_csv(f'{res_dir}/train.csv', index=False)
-test_df.to_csv(f'{res_dir}/test.csv', index=False)
+train_df.to_csv(f'{res_dir}/train.csv')
+test_df.to_csv(f'{res_dir}/test.csv')
