1a5c12955e712aa2e17f1b6778977399182ba86a
Danila <danilaeremenko@mail.ru>
2022-12-08 00:40:30 +0300

    STA: assym function updated (was mixed up with sym)

--- lib/models_building.py
+++ lib/models_building.py
@@ -47,17 +47,20 @@ def get_event_time_manual(event_times: np.ndarray, probs: np.ndarray) -> List[fl
     return res_arr
 
 
+ASSYM_COEF = 10.
+
+
 def assym_obj_fn(y_true, y_pred):
     residual = (y_true - y_pred).astype("float")
-    grad = -2 * residual
-    hess = [2.0] * len(residual)
+    grad = np.where(residual > 0, -2.0 * ASSYM_COEF * residual, -2.0 * residual)
+    hess = np.where(residual > 0, 2.0 * ASSYM_COEF, 2.0)
     return grad, hess
 
 
 def assym_valid_fn(y_true, y_pred):
     residual = (y_true - y_pred).astype("float")
-    loss = residual ** 2
-    return "custom_symmetric_eval", np.mean(loss), False
+    loss = np.where(residual > 0, (residual ** 2.0) * ASSYM_COEF, residual ** 2.0)
+    return "custom_asymmetric_eval", np.mean(loss), False
 
 
 def build_scenarios(
