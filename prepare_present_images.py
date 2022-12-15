3479a67cbca68a5c0a5df0934c0265fb56a97fdd
Danila <danilaeremenko@mail.ru>
2022-12-15 15:07:07 +0300

    STA: multiple examples of each class added into prepare_present_images.py

--- prepare_present_images.py
+++ prepare_present_images.py
@@ -11,7 +11,8 @@ from sksurv.ensemble import RandomSurvivalForest
 from sksurv.util import Surv
 
 from experiments_config import EXP_PATH
-from lib.custom_survival_funcs import add_events_to_df
+from lib.custom_survival_funcs import add_events_to_df, get_event_time_manual
+from lib.drawing import get_random_color
 
 font = {'family': 'Times New Roman',
         'size': 32}
@@ -106,22 +107,22 @@ if __name__ == '__main__':
     y_test_src_df_sel = pd.concat([
         y_test_src_df_sorted[
             (y_test_src_df_sorted['ElapsedRaw'] > 10 * 60) & (y_test_src_df_sorted['ElapsedRaw'] < 20 * 60)
-            & (y_test_src_df_sorted['event'] == event)].iloc[0:1],
+            & (y_test_src_df_sorted['event'] == event)].iloc[[0, 5, 10]],
 
         y_test_src_df_sorted[
             (y_test_src_df_sorted['ElapsedRaw'] > 9 * 3600) & (y_test_src_df_sorted['ElapsedRaw'] < 10 * 3600)
-            & (y_test_src_df_sorted['event'] == event)].iloc[0:1],
+            & (y_test_src_df_sorted['event'] == event)].iloc[[0, 5, 10]],
 
         y_test_src_df_sorted[
             (y_test_src_df_sorted['ElapsedRaw'] > 2 * 24 * 3600) & (y_test_src_df_sorted['ElapsedRaw'] < 3 * 24 * 3600)
-            & (y_test_src_df_sorted['event'] == event)].iloc[0:1]
+            & (y_test_src_df_sorted['event'] == event)].iloc[[0, 5, 10]]
     ]
     )
 
     x_test_sel = x_test.loc[y_test_src_df_sel.index]
     y_pred = rsf.predict_survival_function(x_test_sel, return_array=True)
     ####################################################################################################################
-    # -------------------------------------------------- drawing -------------------------------------------------------
+    # -------------------------------------------------- drawing gif ---------------------------------------------------
     ####################################################################################################################
     fig = plt.figure(figsize=(4 * 5, 3 * 5))
     ax = plt.axes(xlim=(-10, 200), ylim=(-0.1, 1))
@@ -130,37 +131,54 @@ if __name__ == '__main__':
     ax.set_title(f'Функции выживаемости')
     ax.set_xlabel('Время решения (часы)')
     ax.set_ylabel('Вероятность завершения')
-    ax.grid(True)
+    # ax.grid(True)
     # ax.legend(handles=[f"Elapsed time = {task / 3600:.2f}" for task in list(y_test_src_df_sel['ElapsedRaw'])])
     lines = [ax.plot([], [])[0] for i in range(len(y_pred))]
+    GROUP_COLORS = ["#53EF70", "#FFD457", "#FF4848"]
+    COLORS = [group_color for group_color in GROUP_COLORS for repeat in range(3)]
+    assert len(COLORS) == len(y_test_src_df_sel)
+    # COLORS = [get_random_color() for i in range(len(y_test_src_df_sel))]
 
-
-    def init():
-        for line in lines:
-            line.set_data([], [])
-        return lines
-
-
-    COLORS = ["#53EF70", "#FFD457", "#FF4848"]
-
-
-    def animate(i):
-        step_lines = []
-        for pred_i, (line, curr_pred, color) in enumerate(zip(lines, y_pred, COLORS)):
-            x = rsf.event_times_[:i * step] / 3600
-            y = curr_pred[:i * step]
-            label = f"Фактическое время решения = {y_test_src_df_sel.iloc[pred_i]['ElapsedRaw'] / 3600:.2f} часов"
-            step_lines.append(plt.step(x, y, where="post", label=label, linewidth=4, color=color)[0])
-            line.set_data(x, y)
-            # line.set_label(f'Tasks time')
-        ax.legend(handles=step_lines, loc='upper right')
-        fig.tight_layout()
-        return step_lines
-
-
-    step = 40
-    anim = animation.FuncAnimation(fig, animate, init_func=init,
-                                   frames=len(rsf.event_times_) // step, interval=20, blit=True)
-    f = f'rsf {data_type}.gif'
-    writergif = animation.PillowWriter(fps=5)
-    anim.save(f, writer=writergif)
+    ####################################################################################################################
+    # -------------------------------------------------- drawing gif ---------------------------------------------------
+    ####################################################################################################################
+    # def init():
+    #     for line in lines:
+    #         line.set_data([], [])
+    #     return lines
+    #
+    #
+    # def animate(i):
+    #     step_lines = []
+    #     for pred_i, (line, curr_pred, color) in enumerate(zip(lines, y_pred, COLORS)):
+    #         x = rsf.event_times_[:i * step] / 3600
+    #         y = curr_pred[:i * step]
+    #         label = f"Фактическое время решения = {y_test_src_df_sel.iloc[pred_i]['ElapsedRaw'] / 3600:.2f} часов"
+    #         step_lines.append(plt.step(x, y, where="post", label=label, linewidth=4, color=color)[0])
+    #         line.set_data(x, y)
+    #         # line.set_label(f'Tasks time')
+    #     ax.legend(handles=step_lines, loc='upper right')
+    #     fig.tight_layout()
+    #     return step_lines
+    #
+    #
+    # step = 40
+    # anim = animation.FuncAnimation(fig, animate, init_func=init,
+    #                                frames=len(rsf.event_times_) // step, interval=20, blit=True)
+    # f = f'rsf {data_type}.gif'
+    # writergif = animation.PillowWriter(fps=5)
+    # anim.save(f, writer=writergif)
+    ####################################################################################################################
+    # -------------------------------------------------- drawing last frame --------------------------------------------
+    ####################################################################################################################
+    for pred_i, (line, curr_pred, color) in enumerate(zip(lines, y_pred, COLORS)):
+        x = rsf.event_times_ / 3600
+        y = curr_pred
+        y_true = y_test_src_df_sel.iloc[pred_i]['ElapsedRaw'] / 3600
+        y_math_exp = (y * rsf.event_times_).mean() / 3600
+        y_min_prob = get_event_time_manual(event_times=rsf.event_times_, probs=np.expand_dims(y, 0))[0] / 3600
+        label = f"Фактическое время решения = {y_true:.2f} часов"
+        plt.step(x, y, where="post", label=label, linewidth=4, color=color)[0]
+        plt.plot([y_true, y_true], [0, 1], '--', linewidth=2, color=color)
+    plt.legend(loc='upper right')
+    plt.show()
